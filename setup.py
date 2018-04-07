48090041f956b69dceb9d0e7a14bda8998a7e5f6
Alex Jones <38074124+alexjonesphd@users.noreply.github.com>
2018-04-07 11:02:44 +0100

    Added setup py and cfg files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pythonfaceregressor',
+    packages = ['my_python_package'],
+    version = '0.1',
+    description = 'Reconstruct facial appearances from predictor variables',
+    author = 'Alex Jones',
+    author_email = 'alexjonesphd@gmail.com',
+    url = 'https://github.com/alexjonesphd/python-face-regressor',
+    download_url = 'https://github.com/alexjonesphd/python-face-regressor/archive/v0.1.tar.gz',
+    keywords = ['faces', 'regression'],
+    classifiers = [],
+)
