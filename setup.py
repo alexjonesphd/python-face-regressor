7a1988ebe4a67ade2a38e7e223c07e9331780c11
Alex Jones <38074124+alexjonesphd@users.noreply.github.com>
2018-04-14 14:51:15 +0100

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'pythonfaceregressor',
     packages = ['pythonfaceregressor'],
-    version = '0.1',
+    version = '0.2',
     description = 'A package for examining relationships between facial appearance and other variables'
     author = 'Alex Jones',
     author_email = 'alexjonesphd@gmail.com',
