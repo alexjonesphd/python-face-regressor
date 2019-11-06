b044f6d5448db224d2a2896608d9e98dd955ad69
Alex Jones <38074124+alexjonesphd@users.noreply.github.com>
2019-11-06 11:33:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'pythonfaceregressor',
     packages = ['pythonfaceregressor'],
-    version = '6.0',
+    version = '7.0',
     description = 'A package for examining relationships between facial appearance and other variables',
     author = 'Alex Jones',
     author_email = 'alexjonesphd@gmail.com',
     url = 'https://github.com/alexjonesphd/python-face-regressor',
-    download_url = 'https://github.com/alexjonesphd/python-face-regressor/archive/6.0.tar.gz',
+    download_url = 'https://github.com/alexjonesphd/python-face-regressor/archive/7.0.tar.gz',
     keywords = ['faces', 'regression'],
     classifiers = [],
 )
